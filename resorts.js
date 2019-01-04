9864b6370ffe517c14c0d1fc8e0640055986bb51
Colin Dess <pcolind@gmail.com>
2019-01-04 09:58:24 -0800

    adding resort overlay html

---
+++ resorts.js
@@ -0,0 +1,14 @@
+const resorts = {
+	"northstar" : {
+		"map" : "https://assets.vailresorts.com/-/media/northstar/products/brochure/the-mountain/about-the-mountain/trail-map/winter_trail_mapside_201819_no_bleed.ashx",
+		"forecasts" : 4,
+		"locs" : [[31,58,39.25,-120.15, 'backside'],[26,14,39.25,-120.12, 'east ridge'],[70,66,39.27,-120.16, 'lookout mountain'],[79,27,39.28,-120.13, 'base/mid mountain']]
+	},
+
+	"stevens" : {
+		"map" : "https://www.stevenspass.com/site/mountain/maps/trail-map/index.html/TrailMap-Frontside-2017-18.jpg",
+		"forecasts" : 1,
+		"locs" : [[20,42,47.73,-121.09, 'tye bowl']]
+	}
+	
+}
